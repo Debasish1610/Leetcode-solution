927617058fe2e7bb64dc5cef986e1f9ce3500415
Debasish Saha <32010634+Debasish1610@users.noreply.github.com>
2022-09-09 11:05:30 +0600

    Time: 1611 ms (6.73%), Space: 125.1 MB (70.41%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    static bool compare(pair<int,int>a, pair<int,int>b) {
-        if (a.first == b.first) {
-            return a.second < b.second;
+    static bool compare(vector<int>&a, vector<int>&b) {
+        if (a[0] == b[0]) {
+            return a[1] < b[1];
         }
-        return a.first < b.first;
+        return a[0] < b[0];
     }
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
-        vector<pair<int,int>>prop;
-        for (int i = 0; i < properties.size(); i++) {
-            prop.push_back({properties[i][0], properties[i][1]});
-        }
-        sort(prop.begin(), prop.end(), this->compare);
+        sort(properties.begin(), properties.end(), this->compare);
         int weakChars = 0;
         int length = properties.size();
         int lastDifIndex = properties.size();
         for (int i = length-2; i >= 0; i--) {
-            if (prop[i].first != prop[i+1].first) {
+            if (properties[i][0] != properties[i+1][0]) {
                 lastDifIndex = i+1;
             }
             
-            if (lastDifIndex < length && prop[lastDifIndex].second > prop[i].second) {
+            if (lastDifIndex < length && properties[lastDifIndex][1] > properties[i][1]) {
                 weakChars++;
             }
             
-            prop[i].second = max(prop[i].second, prop[i+1].second);
+            properties[i][1] = max(properties[i][1], properties[i+1][1]);
         }
         return weakChars;
     }
