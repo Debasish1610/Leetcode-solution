77d06c92503131bafbeefeaef379b76593dd06dd
Debasish Saha <32010634+Debasish1610@users.noreply.github.com>
2022-09-09 11:18:06 +0600

    Time: 1686 ms (5.05%), Space: 125.2 MB (44.52%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     static bool compare(vector<int>&a, vector<int>&b) {
         if (a[0] == b[0]) {
-            return a[1] < b[1];
+            return a[1] > b[1];
         }
         return a[0] < b[0];
     }
@@ -10,17 +10,17 @@ public:
         sort(properties.begin(), properties.end(), this->compare);
         int weakChars = 0;
         int length = properties.size();
-        int lastDifIndex = properties.size();
-        for (int i = length-2; i >= 0; i--) {
-            if (properties[i][0] != properties[i+1][0]) {
-                lastDifIndex = i+1;
+        int maxValue = -1;
+        for (int i = length-1; i >= 0; i--) {
+            if (properties[i][1] > maxValue) {
+                maxValue =  properties[i][1];
             }
             
-            if (lastDifIndex < length && properties[lastDifIndex][1] > properties[i][1]) {
+            if (maxValue > properties[i][1]) {
                 weakChars++;
             }
             
-            properties[i][1] = max(properties[i][1], properties[i+1][1]);
+            // properties[i][1] = max(properties[i][1], properties[i+1][1]);
         }
         return weakChars;
     }
